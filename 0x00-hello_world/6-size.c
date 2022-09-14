f9acb631ffe8c4ed673c6158a525948011f488c3
Jefferson <enyigideon20@gmail.com>
2022-09-13 21:22:45 -0700

    print sizes of diff. datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
 *main - this program prints the size of different types
 *
-*Description: Different datatypes will be inputed and the computer will pritn their sizes
+*Description: print the size of various datatypes
 *printf - intrisic function to print to screen from stdio.h
 *Return: 0
 */
@@ -16,10 +16,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(c));
-	printf("Size of int: %ld byte(s)\n", sizeof(i));
-	printf("Size of long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
