2a83dcd1dc288a497c0da2b8c49a80495bbb98a6
Jefferson <enyigideon20@gmail.com>
2022-09-13 20:47:30 -0700

    print using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *printf - function to be used to print to screen
+ * Program that prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
