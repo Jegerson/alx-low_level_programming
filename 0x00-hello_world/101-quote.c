20ebeb99b084618641598c604947efe875d2ea86
Jefferson <enyigideon20@gmail.com>
2022-09-14 21:20:13 -0700

    using the write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - function to print a string
+*write - function stdio.h function to be used
+*
+*Description: this program prints without print function
+*Return: 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
