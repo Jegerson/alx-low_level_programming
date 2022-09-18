b19445e9b6bbcd0c88af62fef210ad22caa54ffb
Jefferson <enyigideon20@gmail.com>
2022-09-18 16:12:38 -0700

    all alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - program prints lowercase alphabets
+ *
+ *this prints lowercase leters only in one line
+ *Return; 0
+ */
+
+int main (void)
+{
+	for(int x = 'a'; x <= 'z'; x++) 
+		putchar(x);				
+	return 0;
+}
