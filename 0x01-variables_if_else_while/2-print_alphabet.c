2be84c65bb414a0cfe512b572553ca91d76b44bf
Jefferson <enyigideon20@gmail.com>
2022-09-24 08:44:11 -0700

    code prints all alphabets plus newline

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  *main - program prints lowercase alphabets
  *
  *this prints lowercase leters only in one line
- *Return; 0
+ *Return:0
  */
 
 int main(void)
 {
 	int x;
 
-	for (x = 'a'; x <= 'z'; x++) 
+	for ( x = 'a'; x <= 'z'; x++ ) 
 		putchar(x);
 	putchar('\n');
 	return (0);
