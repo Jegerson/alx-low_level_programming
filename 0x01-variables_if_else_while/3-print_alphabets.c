c982101c831c161424d0e747cbbe8683ca3bcb52
Jefferson <enyigideon20@gmail.com>
2022-09-24 09:39:36 -0700

    lowercase highercase letters with newline

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- *main - program prints lowercase alphabets                                                                                             
+ *main - program prints lowercase alphabets
  *
  *this prints lowercase leters only in one line
  *Return:0
  */
 
-int main(void)   
-
+int main(void)
 {
 	int x;
 	int i;
