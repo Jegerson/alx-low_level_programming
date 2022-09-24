65f8b574314bc48f203d3c9c94844d4b31780c76
Jefferson <enyigideon20@gmail.com>
2022-09-24 10:51:08 -0700

    1 to 9 with newline

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program prints the first 9 numbers
+ *
+ * prints numbers 0 to 9 using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0;i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
